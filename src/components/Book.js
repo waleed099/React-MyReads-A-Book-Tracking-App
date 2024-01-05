d8fbe0b6a0f80dbe696ca356d6d9696f32f1d742
waleedsaeeed <w.saeeed123@gmail.com>
2024-01-06 01:21:14 +0300

    adding a README File to the repository

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,7 @@
 import React from "react";
 import * as BooksAPI from "../BooksAPI";
 
-const Book = props => {
+const Book = (props) => {
   const {
     title,
     authors,
@@ -9,23 +9,31 @@ const Book = props => {
     book,
     setBooks,
     isSearching,
-    bookshelf
+    bookshelf,
   } = props;
 
-  const handleShelfChange = event => {
+  const handleShelfChange = (event) => {
     if (event.target.value !== "move") {
-      BooksAPI.update(book, event.target.value).then(response =>
-        BooksAPI.getAll().then(newBooks => {
+      BooksAPI.update(book, event.target.value).then((response) =>
+        BooksAPI.getAll().then((newBooks) => {
           setBooks(newBooks);
         })
       );
     }
   };
 
-  const handleShelfChangeInSearch = event => {
+  const handleShelfChangeInSearch = (event) => {
     if (event.target.value !== "move") {
       BooksAPI.update(book, event.target.value);
     }
+  };
+
+  const renderShelvesOptions = (shelves) => {
+    return shelves.map((shelf) => (
+      <option key={shelf.id} value={shelf.shelfName}>
+        {shelf.shelfDisplayName} 
+      </option>
+    ));
   };
 
   return (
@@ -36,12 +44,12 @@ const Book = props => {
           style={{
             width: 128,
             height: 193,
-            backgroundImage: `url("${imageUrl}")`
+            backgroundImage: `url("${imageUrl}")`,
           }}
         ></div>
         <div className="book-shelf-changer">
           <select
-            onChange={event => {
+            onChange={(event) => {
               if (!isSearching) {
                 handleShelfChange(event);
               } else {
@@ -53,17 +61,18 @@ const Book = props => {
             <option value="move" disabled>
               Move to...
             </option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read.</option>
-            <option value="read">Read.</option>
+            {renderShelvesOptions([
+              { id: "1", shelfName: "currentlyReading", shelfDisplayName: "Currently Reading" },
+              { id: "2", shelfName: "wantToRead", shelfDisplayName: "Want to Read" },
+              { id: "3", shelfName: "read", shelfDisplayName: "Read" },
+ ])}
             <option value="none">None</option>
+
           </select>
         </div>
       </div>
       <div className="book-title">{title}</div>
-      <div className="book-authors">
-        {authors && authors.map(author => `${author},`)}
-      </div>
+      <div className="book-authors">{authors && authors.map((author) => `${author},`)}</div>
     </div>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,49 +1,53 @@
 import React, { useState, useEffect } from "react";
-
 import { useHistory } from "react-router-dom";
-
+import debounce from "lodash/debounce";
 import Book from "./Book";
-
-// import Footer from "./Footer.js";
-
 import * as BooksAPI from "../BooksAPI";
 
-const Search = props => {
+const Search = (props) => {
   const [searchText, setSearchText] = useState("");
   const [searchedBooks, setSearchedBooks] = useState([]);
   const history = useHistory();
 
-  const handleSearchTextChange = event => {
-    if (searchText.length !== 0) {
-      BooksAPI.search(searchText).then(searchedBooks => {
+  // Debounce the search function
+  const debouncedSearch = debounce((text) => handleSearchTextChange(text), 300);
+
+  const handleSearchTextChange = (text) => {
+    if (text.length !== 0) {
+      BooksAPI.search(text).then((searchedBooks) => {
         if (!searchedBooks.error) {
-          BooksAPI.getAll().then(myBooks => {
-            setSearchedBooks(setDefaultShelves(searchedBooks, myBooks));
+          BooksAPI.getAll().then((myBooks) => {
+            const xxx = setDefaultShelves(searchedBooks, myBooks);
+            setSearchedBooks(xxx); 
           });
         } else {
           setSearchedBooks([]);
         }
       });
-    } else if (searchText.length === 0) {
+    } else if (text.length === 0) {
       setSearchedBooks([]);
     }
   };
 
   const setDefaultShelves = (searchedBooksLocal, myBooks) => {
-    return searchedBooksLocal.map(book => {
-      for (let i = 0; i < myBooks.length; i++) {
-        if (myBooks[i].id === book.id) {
-          return { ...book, shelf: myBooks[i].shelf };
-        }
-      }
-      return { ...book, shelf: "none" };
+    return searchedBooksLocal.map((searchBook) => {
+      const bookFound = myBooks.find((book) => book.id === searchBook.id);
+  
+      const updatedBook = bookFound
+        ? { ...searchBook, shelf: bookFound.shelf }
+        : { ...searchBook, shelf: "none" };
+  
+  
+      return updatedBook;
     });
   };
 
   useEffect(() => {
-    handleSearchTextChange();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchText]);
+    debouncedSearch(searchText);
+    return () => {
+      debouncedSearch.cancel();
+    };
+  }, [searchText, debouncedSearch]);
 
   return (
     <div className="search-books">
@@ -55,29 +59,35 @@ const Search = props => {
           <input
             type="text"
             placeholder="Search by title or author"
-            onChange={event => setSearchText(event.target.value)}
+            onChange={(event) => setSearchText(event.target.value)}
           />
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid">
-          {searchedBooks &&
-            searchedBooks.map((book, index) => (
-              <Book
-                key={index}
-                title={book.title}
-                authors={book.authors}
-                imageUrl={book.imageLinks && book.imageLinks.thumbnail}
-                bookshelf={book.shelf}
-                book={book}
-                isSearching
-              />
-            ))}
-        </ol>
+      <ol className="books-grid">
+  {searchedBooks &&
+    searchedBooks.map((book, index) => (
+      <Book
+        key={index}
+        title={book.title}
+        authors={book.authors}
+        imageUrl={book.imageLinks && book.imageLinks.thumbnail}
+        bookshelf={book.shelf}
+        book={book}
+        isSearching
+      />
+    ))}
+</ol>
       </div>
-      {/* <Footer /> */}
     </div>
   );
 };
 
 export default Search;
+
+
+
+
+
+
+
